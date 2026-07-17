395f45b38d082cc79292f12193c4466af82d9813
Adhikari-Satish <adhikarisatish62@gmail.com>
2026-07-17 13:13:09 +0530

    Setup PostgreSQL database with SQLAlchemy ORM

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,7 +6,9 @@ class Settings(BaseSettings):
     APP_NAME: str = "AI-Chatbot"
 
     VERSION: str = "1.0.0"
-
+    
+    DATABASE_URL: str
+    
     DEBUG: bool = True
 
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.db.database import engine, Base
+
+from app.models import user
+
+Base.metadata.create_all(
+    bind=engine
+)
 
 app = FastAPI(
     title=settings.APP_NAME,
